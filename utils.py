d05604175082f928e1ea2f52ba284c19a483dc2f
Mahen Gandhi <mahengandhi19@gmail.com>
2020-09-18 20:27:11 +0530

    added resnet in pytorch

--- utils.py
+++ utils.py
@@ -1,14 +1,16 @@
-import torch
-from sklearn.model_selection import train_test_split
-from torch.utils.data import Dataset, DataLoader
-from local_settings import DATA_DIR
-import torchvision
 import multiprocessing as mp
-import numpy as np
 import os
-from tqdm import tqdm
+
+import numpy as np
 import pandas as pd
+import torch
+import torchvision
 from skimage import io
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Dataset
+from tqdm import tqdm
+
+from local_settings import DATA_DIR
 
 BATCH_SIZE = 32
 SIZE = (224, 224)
@@ -16,8 +18,7 @@ SIZE = (224, 224)
 TRANSFORMS = torchvision.transforms.Compose([
     torchvision.transforms.Resize(SIZE),
     torchvision.transforms.ToTensor(),
-    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225])
+    torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
 
@@ -35,7 +36,7 @@ def make_df():
 
 class C101Dataset(Dataset):
     def __init__(self, data, transform=TRANSFORMS):
-        self.data = self.data
+        self.data = data
         self.transform = transform
 
     def __len__(self):
@@ -48,7 +49,10 @@ class C101Dataset(Dataset):
         img_path = os.path.join(DATA_DIR, self.data.iloc[idx, 0])
         image = io.imread(img_path)
         label = self.data.iloc[idx, 1]
-        sample = {'image': image, 'label': label}
+        sample = {
+            'image': image,
+            'label': label
+        }
 
         if self.transform:
             sample = self.transform(sample)
@@ -68,6 +72,10 @@ def load_dataset(df):
     return loader
 
 
-full_data = make_df()
-train, test = train_test_split(full_data, test_size=0.2)
-train, test = load_dataset(train), load_dataset(test)
+def get_datasets():
+    full_data = make_df()
+    classes = set(full_data['label'].tolist())
+    train, temp = train_test_split(full_data, test_size=0.3)
+    val, test = train_test_split(temp, test_size=0.5)
+
+    return classes, load_dataset(train), load_dataset(val), load_dataset(test)
